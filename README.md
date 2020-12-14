89a57ab8f8386059bf7f6bf73049bb2c150056dc
Yao Yi <29729108@qq.com>
2020-12-14 14:13:26 +0800

    the end

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-# yunxi
-
 ## Build
+这是一个基于webpack的多页面打包工具
+它能自动生成html文件，并自动添加所需的依赖项
+虽然有很多的地方需要完善，但我们并不想更新它
+因为webpack已经被我们抛弃了
+请看下一版本
+你会看到一个全新的打包工具。
 
 ``` bash
 # 安装依赖
 npm install
 
 # 打包js and css
-npm run start
+uimi build
 
 # 运行服务测试
-npm run server
-
-# 打包js
-npm run build
+uimi server
 ```
--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,15 +1,15 @@
-import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
 export default {
-  input: "./lib/index.js",
+  input: "lib/index.js",
   output: [
     {
-      file: "./dist/node/cli/index.js",
-      format: 'cjs', interop:false
-    
+      file: "dist/node/cli/index.js",
+      format: 'cjs',
+      interop: false
       //sourcemap: true
     }
   ],
   plugins: [
-    json()
+    terser()
   ]
 }
